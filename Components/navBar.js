92f1c2a1147dacd3fe11b5974c0aa0e855eeba22
MedHachami <moha.hach.9@gmail.com>
2023-10-20 23:17:21 +0200

    Update navBar.js

--- Components/navBar.js
+++ Components/navBar.js
@@ -1,12 +1,12 @@
 function generateNavbar() {
     return `
     <nav class="navbar">
-        <a href="/"><img src="./assets/Console_logo.png" alt="logo" class="logo"></a>
+        <a href="./"><img src="./assets/Console_logo.png" alt="logo" class="logo"></a>
         <img src="./assets/menu-regular-24.png" alt="" class="menu-icon" id="menu-icon"  onclick="toggleSidebar()">
         <div class="sidebar" id="sidebar">
             <ul class="nav-ietms">
-                <li class="nav-item"><a href="" class="nav-link">Home</a></li>
-                <li class="nav-item"><a href="" class="nav-link">How It Work</a></li>
+                <li class="nav-item"><a href="./" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="./how-it-work.html" class="nav-link">How It Work</a></li>
                 <li class="nav-item"><a href="" class="nav-link">Store</a></li>
                 
             </ul>
@@ -22,3 +22,15 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    var menuIcon = document.getElementById("menu-icon"); 
+
+    if (sidebar.style.display === "list-item") {
+        sidebar.style.display = "none";
+        menuIcon.src = "../assets/menu-regular-24.png";
+    } else {
+        sidebar.style.display = "list-item"; 
+        menuIcon.src = "../assets/x-regular-24.png";
+    }
+}
