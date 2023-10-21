1af7e77a99ebfbfb02da5ba5c37c98a03af036cb
MedHachami <moha.hach.9@gmail.com>
2023-10-21 19:45:00 +0200

    adding stroe page

---
+++ store.php
@@ -0,0 +1,78 @@
+<?php
+    
+
+
+    
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/index.css">
+    <link rel="stylesheet" href="./style/store.css">
+    <title>Store</title>
+</head>
+<body>
+    <div id="navbar"></div>
+    <div class="store">
+        <div class="input-container">
+            <form action="" method="POST">
+                <select name="productType" onchange="this.form.submit()">
+                    <option value="all">ALL products</option>
+                    <option value="games">Games</option>
+                    <option value="magazine">Magazine</option>
+                </select>
+            </form>
+            
+        </div>
+        <div class="store-cards">
+        <?php
+                
+            if(isset($_POST['productType'])){
+            include_once('./includes/fetchProductByType.inc.php');
+            $productType = $_POST['productType'];
+            $products1 = getProductByType($productType);
+            foreach($products1 as $product){ 
+        ?>
+            <div class="store-card">
+                <h1><?php echo $product['title'] ?></h1>
+                <div class="card-thumb">
+                    <img src="./assets/<?php echo $product['imgUrl'];?>" alt=""  >
+                </div>
+                <p><?php echo $product['description1'] ?></p>
+                <div class="storeCard-footer">
+                    <a href="">Buy Now</a>
+                    <span>$ <?php echo $product['price'] ?></span>
+                </div>
+            </div>
+            <?php }} else{?>
+            <?php
+                include_once('./includes/fetchProducts.inc.php');
+                $products = getAllProducts();
+                foreach($products as $product){ 
+            ?>
+            <div class="store-card">
+                <h1><?php echo $product['title'] ?></h1>
+                <div class="card-thumb">
+                    <img src="./assets/<?php echo $product['imgUrl'];?>" alt=""  >
+                </div>
+                <p><?php echo $product['description1'] ?></p>
+                <div class="storeCard-footer">
+                    <a href="">Buy Now</a>
+                    <span>$ <?php echo $product['price'] ?></span>
+                </div>
+            </div>
+            <?php }}?>
+            
+            
+        </div>
+        
+    </div>
+    
+    <div id="footer"></div>
+    <script src="./Components/Footer.js"></script>
+    <script src="./Components/navBar.js"></script>
+</body>
+</html>
