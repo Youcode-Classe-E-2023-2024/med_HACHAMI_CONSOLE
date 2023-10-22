38f6bfb639297a3351d07cb7ea94c57180aac499
MedHachami <moha.hach.9@gmail.com>
2023-10-22 18:35:36 +0200

    Create edit.inc.php

---
+++ Dashboard/includes/edit.inc.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_POST['submit']))
+{
+    //Grabing the data
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $stock_quantity = $_POST['stock_quantity'];
+    $reviews = $_POST['reviews'];
+    $product_code = $_POST['product_code'];
+    include "../classes/dbh.classes.php";
+    include "../classes/product.classes.php";
+    include "../classes/productEdit.contr.php";
+   
+    $product = new Product();
+    $product->editeProduct($title,$description,$price, $stock_quantity,$reviews,$product_code);
+   
+    
+
+}
