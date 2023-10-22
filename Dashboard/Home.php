26398848aa038d8f6bba50d7fa73f224c95bb3f7
MedHachami <moha.hach.9@gmail.com>
2023-10-23 00:40:16 +0200

    deleting path

--- Dashboard/Home.php
+++ Dashboard/Home.php
@@ -14,6 +14,8 @@ if (!isset($_SESSION["user_id"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="style/home.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
     
 </head>
 <body>
@@ -53,19 +55,29 @@ if (!isset($_SESSION["user_id"])) {
                 <div class="table-data"><?php echo $product['reviews']; ?></div>
                 <div class="table-data actions">
                     <a href="edit.php?product_code=<?php echo $product['product_code'];?>"  ><img src="./assets/edit-icon.png" class="action-icon"></a>
-                    <a href="delete.php?product_code=<?php echo $product['product_code']; ?>"  ><img src="./assets/icon-delete.png" class="action-icon"></a>
+                    <a href="delete.php?product_code=<?php echo $product['product_code'];?>" id="myBtn" onclick="openModel('<?php echo $product['product_code']; ?>')" ><img src="./assets/icon-delete.png" class="action-icon"></a>
                 </div>
+                
            
             </div>
+            
             <?php } ?>
         </div>
-        
+      
         
         
     </table>
-                
-    <script src="./Components/navbar.js"></script>
-    <script src="./Components/sideBar.js"></script>
+        
+    
 </div>
+
+
+
+
+
+
+<script src="./Components/navbar.js"></script>
+<script src="./Components/sideBar.js"></script>
+
 </body>
 </html>
