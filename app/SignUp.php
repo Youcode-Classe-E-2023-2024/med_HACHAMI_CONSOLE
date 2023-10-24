3ad063d56a90b5ccf43174c2677f675fcfb3bda0
MedHachami <moha.hach.9@gmail.com>
2023-10-24 04:18:08 +0200

    Create SignUp.php

---
+++ app/SignUp.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign-Up</title>
+    <link rel="stylesheet" href="./style/index.css">
+    <link rel="stylesheet" href="./style/signUp.css">
+   
+</head>
+<body>
+    <div id="navbar"></div>
+
+  
+<div class="login-container">
+ <section> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> 
+
+<div class="signin"> 
+
+ <div class="content"> 
+
+  <h2>Register Now</h2> 
+
+  <form class="form" action="includes/singup.inc.php" method="POST" > 
+
+   <div class="inputBox inputBox1"> 
+
+    <input type="text" placeholder="Full Name" name="fullname" required> 
+    <input type="text" placeholder="Email" name="email" required> 
+   </div> 
+   
+   <div class="inputBox "> 
+
+    <input type="text" name="phone" required> <i>Phone number</i> 
+
+   </div>
+
+   <div class="inputBox"> 
+
+    <input type="password" name="pwd" required> <i>Password</i> 
+
+   </div> 
+   <div class="inputBox"> 
+
+    <input type="password" name="pwdrepeat" required> <i>Repeat Password</i> 
+
+   </div> 
+
+   <div class="links"> <a href="#">Already Have an account ?</a> <a href="#">Login</a> 
+
+   </div> 
+
+   <div class="inputBox"> 
+
+    <!-- <input type="submit" value="Login">  -->
+    <button class="register-btn" name="submit" type="submit" >Register</button>
+   </div> 
+
+</form> 
+
+ </div> 
+
+</div> 
+
+</section>
+</div>
+
+    <div id="footer"></div>
+    <script src="./Components/navBar.js"></script>
+    <script src="./Components/Footer.js"></script>
+ 
+    </script>
+</body>
+</html>
